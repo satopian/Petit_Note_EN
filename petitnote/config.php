20776a7259f2111be06ba6c44eb194a0591d4fbf
satopian <satopian@mia.eek.jp>
2022-12-17 23:26:42 +0900

    config.php English version.

--- petitnote/config.php
+++ petitnote/config.php
@@ -1,331 +1,334 @@
 <?php
-/*設定項目*/
+/*Setting items*/
 
-/*-----絶対に変更が必要な項目-----*/
+/*------Required setting items-----*/
 
-//管理者パスワード 必ず変更してください。
-$admin_pass = "kanripass";
+//Be sure to change the admin password.
+$admin_pass = "adminpass";
 
-//第2パスワード 必ず変更してください。
-//管理者投稿や管理者削除の時に管理者である事を再確認する為に使うパスワード。
-//内部で処理するため覚えておく必要はありません。
-//管理パスと同じパスワードは使えません。
+//Please change the second password.
+// Password to double check that you are an admin when posting or deleting with admin privileges.
+// You don't have to remember it because it's handled internally.
+//The password cannot be the same as the management path.
 $second_pass = "mGeL5dhpQ8e9ugd";
 
-//この掲示板の名前
+// name of this board
 $boardname = "Petit Note";
 
-//ホームページ(掲示板からの戻り先)
-$home = "./"; //相対パス、絶対パス、URLどれでもOK 
+// home page (return destination from the bulletin board)
+$home = "./"; //relative path, absolute path, URL any
 
-//最大スレッド保存件数 この数値以上のスレッドは削除されます
-//最低500スレッド。
-$max_log = 3000;
+// maximum number of threads that can be stored
+// 500 threads minimum.
+$max_log = 1000;
 
-// 設置場所のurl `/`まで。
+// Up to the installation destination URL `/`.
 $root_url = "http://example.com/oekaki/";
 
-//名前を必須にする
-// しない: false の時に名前を空欄で投稿すると、｢anonymous｣になります。
-// する: true しない: false
+// require name
+// None: Posting an empty name when false makes it "anonymous".
+// yes: true no: false
 
 $name_input_required = true;
 // $name_input_required = false;
 
-//本文の制限文字数。半角で
+// Limit the number of characters in the body. (unit byte)
 $max_com=1000;
 
-/*-----絶対に変更が必要な項目ここまで-----*/
+/*-----This is the end of the required setting items. -----*/
 
-/*SNS連携*/
+/* SNS linkage */
 
-// ツイートボタンを使う
-// スレッドのurlと内容をツイートするリンクがでます。
-// 使う: true 使わない: false
+// use tweet button
+// Display the URL of the thread and a link to tweet the content.
+// use: true not use: false
 
 $use_sns_button = true;
 // $use_sns_button = false;
 
-/*テンプレート切り替え*/
-//テンプレートのディレクトリ`/`まで 初期値 "basic/"
+/* Switch template */
+// up to the template directory `/` initial value "basic/"
 $skindir="basic/";
 
-/*掲示板の説明文*/
+/* message board description */
 
-// テンプレートに直接記入しても構いませんが、ここで入力する事もできます。
-// 説明文が1行なら ["説明そのいち"]
-// 説明文が3行なら ["説明そのいち","説明そのに","説明そのさん"]
-// 文字をダブルクオートで囲って、カンマで区切ります。
-// 説明文が不要なら []で。
+// You can enter it directly in the template, but you can also enter it here.
+// If the description is one line ["Description 1"]
+// If the description is 3 lines ["Description 1", "Description 1", "Description 3"]
+// Enclose the characters in double quotes and separate them with commas.
+// Use [] if no description is needed.
 
-// $descriptions = ["iPadやスマートフォンでも描けるお絵かき掲示板です。"];	
-$descriptions = ["iPadやスマートフォンでも描けるお絵かき掲示板です。","楽しくお絵かき。"];	
+// $descriptions = ["This is a drawing board that allows you to draw on your iPad or smartphone. "];
+$descriptions = ["This is a drawing board that allows you to draw on your iPad or smartphone. ","Let's have fun. "];
 
+/* email notification */
 
-/*メール通知*/
-
-// 投稿をメールで通知する
-// する: true しない: false
+// Notify posts by email
+// yes: true no: false
 
 // $send_email = true;
 $send_email = false;
 
-//投稿があった事を通知するメールアドレス
+// Email address to notify that there was a post
 $to_mail = "example@example.com";
 
-/*スパム対策*/
-//本文に日本語がなければ拒絶 する:true しない:false
-$use_japanesefilter = true;
-// $use_japanesefilter=false;
+/* anti spam */
+//Reject if text does not contain Japanese
+//Reject: true Do not reject: false
 
-//拒絶する文字列 正規表現
-// 設定しないなら[]で。
-$badstring = ["example.example.com","未承諾広告"];
+// $use_japanesefilter = true;
+$use_japanesefilter=false;
 
-//拒絶するurl
+//Reject if this string exists
+// If not set, use [].
+$badstring = ["example.example.com","Unsolicited ads"];
+
+//URLs to deny
 $badurl = ["example.com","www.example.com"];
 
-//使用できない名前 正規表現
-$badname = ["ブランド","通販","販売","口コミ"];
+//unusable name
+$badname = ["brand","mail order","sale","review"];
 
-//AとBが両方あったら拒絶 正規表現
-$badstr_A = ["激安","低価","コピー","品質を?重視","大量入荷"];
-$badstr_B = ["シャネル","シュプリーム","バレンシアガ","ブランド"];
+// Reject if both A and B exist
+$badstr_A = ["Cheap","Low Price","Copy","Quality Focus","Large Inventory"];
+$badstr_B = ["Chanel","Supreme","Balenciaga","Brand"];
 
-//禁止ホスト
+// hostname to reject
 $badhost =["example.com","example.org"];
 
-/*使用目的別設定*/
+/*Settings by Purpose of Use*/
 
-// 本文へのURLの書き込みを許可する
-// URLを書き込むスパムを排除する時は しない: false
-// 管理者は設定に関わらず本文にURLを書き込めます。
-// する: true しない: false
+// allow writing URLs in the body
+// to filter spam that writes a URL: false
+// Admins can write URLs in the body regardless of the setting.
+// yes: true no: false
 
-// $allow_comments_url = true; 
-$allow_comments_url = false; 
+// $allow_comments_url = true;
+$allow_comments_url = false;
 
-// URLを自動リンクする
-// マークダウン記法も使えます。[リンクの文字](https://example.com/)
-// する: true しない: false
+// Autolink the URL
+// Markdown notation can also be used. [link text](https://example.com/)
+// yes: true no: false
 
 $use_autolink = true;
 // $use_autolink = false;
 
-//トップページの入力フォームを使用する
-// しない: false でお絵かきボタンのみの表示になります。
-// する: true しない: false
+// Use the input form on the top page
+// No: If False, only draw buttons are visible.
+// yes: true no: false
 
 $use_top_form = true;
 // $use_top_form = false;
 
-//画像アップロード機能を使う
-//管理者投稿モード(日記)でログインしている時は使わないに設定しても、ファイルアップロードが可能です。
-// 使う:true 使わない:false
+// use image upload function
+//If you are logged in with administrator post mode (diary), you can upload files even if you set it to not use.
+// use: true not use: false
 
 $use_upload = true;
 // $use_upload = false;
 
-//レスで画像アップロード機能を使う
-//管理者投稿モード(日記)でログインしている時は使わないに設定しても、レスでお絵かきやレス画像のファイルアップロードが可能です。
-// 使う: true 使わない: false
+// use image upload in reply form
+//If you are logged in in admin posting mode (diary mode), you can draw pictures and upload photo files even if you set it to not use.
+
+// use: true not use: false
 
 $use_res_upload = true;
-// $use_res_upload =  false;
+// $use_res_upload = false;
 
-//コメントのみの新規投稿を許可する、しない。
-//しない: false で、スレ立てに画像が必須になります。
-// する: true しない: false
+// Whether to allow comment-only posts on new posts.
+// None: false, an image is required when creating a new thread.
+// yes: true no: false
 
-// $allow_coments_only = true;
-$allow_coments_only = false;
+// $allow_comments_only = true;
+$allow_comments_only = false;
 
-//日記モードを使用する
-//する: true でスレッド立ては管理者のみになります。
-// する: true しない: false
+// use diary mode
+// Yes: if true, new thread creation is restricted to admins only.
+// yes: true no: false
 
 // $use_diary = true;
 $use_diary = false;
 
-//返信を管理者のみに限定する
-//する: true で管理者以外返信ができなくなります。
-//日記モードと併用すれば、すべての書き込みが管理者のみになります。
+// Only admins can reply
+// Yes: true if only admins can reply.
+//When used with diary mode, all entries are admin-only posts.
 
 // $only_admin_can_reply = true;
 $only_admin_can_reply = false;
 
-//合言葉機能を使って投稿を制限する
-// する: true しない: false
+// use the secret word feature to restrict posting
+// yes: true no: false
 
 // $use_aikotoba = true;
 $use_aikotoba=false;
 
-//合言葉機能で投稿を制限する時の合言葉
-//この合言葉が入力されていない時には書き込むことができません。
-// 必要に応じて変更してください。
-$aikotoba = "あいうえお";
+// Restrict posts with a secret word.
+// Cannot write unless this secret word is entered.
+// change as needed.
+$aikotoba = "secret";
 
-//年齢制限付きの掲示板として設定する
-//する: trueに設定すると確認ボタンを押すまで画像にぼかしが入ります。
-// する: true しない: false
+// set this entire board as NSFW content
+// Yes: If set to true, the image will be blurred until the confirm button is pressed.
+// yes: true no: false
 
 // $set_nsfw = true;
 $set_nsfw = false;
 
-// 閲覧注意を設定する
-//する: trueに設定すると閲覧注意の設定ができるようになります。閲覧注意画像にぼかしが入ります。
-// する: true しない: false
+// Sensitive content settings
+
+// Set sensitive content for each article
+// Yes: Set to true to set sensitive content warnings. Blur images of sensitive content.
+// yes: true no: false
 
 // $mark_sensitive_image = true;
 $mark_sensitive_image = false;
 
-//編集しても投稿日時を変更しないようにする 
-//日記などで日付が変わると困る人のための設定
-//する: trueに設定すると編集しても投稿日時が変わりません。 通常は しない: false 。
-// する: true しない: false
+// don't change post date when editing
+// Settings when you do not want to change the date for diary purposes, etc.
+// Yes: If set to true, editing will not change the post date. Normally none: false .
+// yes: true no: false
 
 // $do_not_change_posts_time = true;
 $do_not_change_posts_time = false;
 
-//レスがついてもスレッドがあがらないようにする
-//する: trueに設定するとレスがついてもスレッドがあがりません。(全てsage)。
-//初期値 false
+// all sage
+// yes: set to true to prevent thread position from changing on replies. All will be treated as sage.
+// initial value false
 
 //$sage_all = true;
 $sage_all = false;
 
-//管理者を認証する
-//する: true で、管理者の投稿の時は認証マークが出ます。初期テンプレートではチェックマーク。
-//管理者モードでログイン、またはパスワード一致の時に管理者と判定します。
-// する: true しない: false
+// Add admin mark to posts by admin.
+// yes: yes, add admin mark to admin posts.
+//If you log in admin mode or if the passwords match, you are considered an admin.
+// yes: true no: false
 
-$verified_adminpost = true; 
-// $verified_adminpost = false; 
+$verified_adminpost = true;
+// $verified_adminpost = false;
 
-//レス画面に前後のスレッドの画像を表示する する:1 しない:0
-// する: true しない: false
+// Display images of the previous and next threads on the screen below the thread Yes: true No: false
+// yes: true no: false
 
 $view_other_works = true;
 // $view_other_works = false;
 
 
-//管理者ページに最新のリリースのバージョンとリンクを表示する
-// する: true しない: false
+// show latest release version and link on admin page
+// yes: true no: false
 
 $latest_var = true;
 // $latest_var = false;
 
-/*表示件数*/
+/* display result */
 
-//1ページに表示するスレッド数
+//Number of threads to display on one page
 
 $pagedef = 10;
 
-//1スレッドに返信できるレスの数
-//管理者による投稿はこの制限を受けません。
+// Number of replies that can be posted in one thread
 
 $max_res = 100;
 
-//1スレッドに表示するレスの数
-//返信画面では全て表示します。
+// number of reply to display in one thread
+// Display all when reply screen.
 
-$dispres= 5;
+$display=5;
 
-//カタログモード時の1ページあたりの表示件数
-//6の倍数で設定すると画面にきれいにおさまります。
+// number of items displayed per page in catalog mode
+// Set it in multiples of 6 to fit nicely on the screen.
 
 $catalog_pagedef = 24;
 
-/*画像関連*/
+/* image related */
 
-//投稿できる画像の容量上限 単位kb
+//Maximum size of images that can be posted (kb)
 
 $max_kb = 2048;
 
-//投稿できる画像の幅と高さの上限 単位px これ以上は縮小
-//縮小されるのはアップロード画像のみ。お絵かきの制限値はここのすぐ下の設定項目で。
-
+//Specify the maximum width and height of the image that can be posted in px.
+// Only uploaded images are scaled down. Set the maximum drawing size in another item.
 $max_px = 1024;
 
-//お絵かきできる幅と高さの最大サイズ
+// Maximum size of width and height that can be drawn
 
-$pmax_w = 800;//幅
-$pmax_h = 800;//高さ
+$pmax_w = 800;//width
+$pmax_h = 800;//height
 
-//スレッドの親の表示する幅と高さの最大サイズ
+// Maximum size of width and height of the image displayed when the thread is parent
 
-$max_w = 800;//幅
-$max_h = 800;//高さ
+$max_w = 800;//width
+$max_h = 800;//height
 
-//スレッドのレスの表示する幅と高さの最大サイズ
+// maximum size of width and height  of the image displayed when replying thread
+$res_max_w = 300;//width
+$res_max_h = 300;//height
 
-$res_max_w = 300;
-$res_max_h = 300;
-
-//表示する幅と高さの最大サイズを超える時はサムネイルを作成する
-// する: true しない: false
+// create a thumbnail if it exceeds the maximum width and height to display
+// yes: true no: false
 
 $use_thumb = true;
 // $use_thumb = false;
 
-/*セキュリティ*/
+/* safety */
 
-// 管理者パスワードを5回連続して間違えた時は拒絶する
-// する: true しない: false
-// trueにするとセキュリティは高まりますが、ログインページがロックされた時の解除に手間がかかります。
+//Reject if admin password is wrong for her 5 times in a row
+// yes: true no: false
+// yes: true for more security, but if the login page is locked it will take more effort to unlock it.
 
 // $check_password_input_error_count = true;
 $check_password_input_error_count = false;
 
-//ftp等でアクセスして、
-// `template/errorlog/error.log`
-// を削除すると、再度ログインできるようになります。
-//このファイルには、間違った管理者パスワードを入力したクライアントのIPアドレスが保存されています。
+// Access via ftp etc.
+// Remove the `template/errorlog/error.log`
+// and you should be able to login again.
+// This file contains the IP addresses of clients who entered an incorrect admin password.
 
-/*詳細設定*/
 
-//古いスレッドを自動的に閉じる日数 単位 日
-//古いスレッドへのスパム防止
-//初期設定の180で、半年前に立てられたスレッドに返信できなくなります。
-//日数による制限をしない時は 0 。 
-//管理者投稿はこの制限を受けません。
+/*Advanced Setting*/
+
+// days in days before automatically closing old threads
+// prevent spamming old threads
+// For example, the default setting of 180 prevents users from replying to threads that were created half a year ago.
+// 0 for unlimited days.
 
 $elapsed_days=180;
 
-//すべての投稿を拒否する
-//管理人長期不在、展示のみなど。
-//する: trueで、すべての投稿ができなくなります。 初期値 false。
-// する: true しない: false
+// reject all posts
+// Set this if the administrator is away for a long time or wants to display only.
+// Yes: true, disallow all posts. Initial value false.
+// yes: true no: false
 
 // $deny_all_posts = true;
 $deny_all_posts = false;
 
 
-//タイムゾーン 日本時間で良ければ初期値 "asia/tokyo"
+// default value of timezone is "asia/tokyo"
+// Please change it according to your living area.
 
 date_default_timezone_set("asia/tokyo");
 
-//iframe内での表示を 拒否する:true 許可する:false
-//セキュリティリスクを回避するため "拒否する:true" を強く推奨。
+// Deny display in iframe: true Allow: false
+// "Deny: true" is highly recommended to avoid security risks.
 
 $x_frame_options_deny=true;
 // $x_frame_options_deny=false;
 
-//通常は変更しません
-//ペイント画面の$pwdの暗号化
 
-define("CRYPT_PASS","v25Xc9nZ82a5JPT");//暗号鍵初期値
+/* usually don't change */
+
+// Encrypt the $pwd of the paint screen
+
+define("CRYPT_PASS","v25Xc9nZ82a5JPT");//Crypt key initial value
 define("CRYPT_METHOD","aes-128-cbc");
-define("CRYPT_IV","T3pkYxNyjN7Wz3pu");//半角英数16文字
+define("CRYPT_IV","T3pkYxNyjN7Wz3pu");//16 single-byte alphanumeric characters
 
-/*変更不可*/
+/* Do not change this part */
 
-//変更しないでください
-//テンポラリ
+// temporary
 define("TEMP_DIR","temp/");
-//ログ
+//log
 define("LOG_DIR","log/");
-//画像
+//image
 define("IMG_DIR","src/");
-//画像
+//thumbnail
 define("THUMB_DIR","thumbnail/");
