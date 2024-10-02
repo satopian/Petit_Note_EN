5ee7cc9953188abc9f0409d3d05eb453f8e9bef5
satopian <satopian@mia.eek.jp>
2024-10-02 21:10:49 +0900

    v1.53.3

--- petitnote/config.php
+++ petitnote/config.php
@@ -500,14 +500,15 @@ $misskey_servers=
 	["misskey.io","https://misskey.io"],
 	["misskey.design","https://misskey.design"],
 	["nijimiss.moe","https://nijimiss.moe"],
-	["sushi.ski","https://sushi.ski"],
 	["misskey.art","https://misskey.art"],
 	["oekakiskey.com","https://oekakiskey.com"],
 	["misskey.gamelore.fun","https://misskey.gamelore.fun"],
 	["novelskey.tarbin.net","https://novelskey.tarbin.net"],
 	["tyazzkey.work","https://tyazzkey.work"],
+	["sushi.ski","https://sushi.ski"],
 	["misskey.delmulin.com","https://misskey.delmulin.com"],
 	["side.misskey.productions","https://side.misskey.productions"],
+	["mk.shrimpia.network","https://mk.shrimpia.network"],
 
 ];
 
