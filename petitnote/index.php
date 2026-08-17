ff4a6624dd22b1784b984afb864c12b79a2a7cc6
satopian <satopian@mia.eek.jp>
2026-08-18 00:36:00 +0900

    misskeyサーバ直接入力を廃止。

--- petitnote/index.php
+++ petitnote/index.php
@@ -3,8 +3,8 @@
 //https://paintbbs.sakura.ne.jp/
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 
-$petit_ver='v3.7.2';
-$petit_lot='lot.20260816';
+$petit_ver='v3.8.1';
+$petit_lot='lot.20260817';
 
 $lang = ($http_langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '')
   ? explode( ',', $http_langs )[0] : '';
@@ -20,13 +20,13 @@ if(!is_file(__DIR__.'/functions.php')){
 	die(__DIR__.'/functions.php'.($en ? ' does not exist.':'がありません。'));
 }
 require_once(__DIR__.'/functions.php');
-if(!isset($functions_ver)||$functions_ver<20260814){
+if(!isset($functions_ver)||$functions_ver<20260817){
 	die($en?'Please update functions.php to the latest version.':'functions.phpを最新版に更新してください。');
 }
 
 check_file(__DIR__.'/misskey_note.inc.php');
 require_once(__DIR__.'/misskey_note.inc.php');
-if(!isset($misskey_note_ver)||$misskey_note_ver<20260714){
+if(!isset($misskey_note_ver)||$misskey_note_ver<20260817){
 	die($en?'Please update misskey_note.inc.php to the latest version.':'misskey_note.inc.phpを最新版に更新してください。');
 }
 
@@ -1043,8 +1043,8 @@ function paint(): void{
 	}
 
 	$hide_animation = false;
-
 	$rep = false;
+
 	/** 続きを描く*/
 	$mode = (string)filter_input_data('POST', 'mode');
 	if ($mode === "contpaint") {
